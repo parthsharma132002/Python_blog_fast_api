f26d9d0ffc05d55b87a74dd5d72dcda343752427
parthsharma132002 <parthsharmappp@gmail.com>
2025-01-03 14:00:53 +0530

    updated code

--- src/blog/controller.py
+++ src/blog/controller.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import Session
 from src.database import sessionLocal
 from src.blog import schema, serializer
 
+from src.blog.model import Blog 
+
 router = APIRouter()
 
 def get_db():
@@ -12,45 +14,34 @@ def get_db():
     finally:
         db.close()
 
-# to create data
-@router.post("/blogs/", response_model=schema.Blog)
-async def create_blog(blog: schema.BlogCreate, db: Session = Depends(get_db)):
-    return serializer.create_blog(db=db, blog=blog)
-
-# to get specific data
-@router.get("/blogs/{blog_id}", response_model=schema.Blog)
-async def read_blog(blog_id: int, db: Session = Depends(get_db)):
-    db_blog = serializer.get_blog(db=db, blog_id=blog_id)
+# get a blog by its ID
+async def get_blog_by_id(blog_id: int, db: Session):
+    db_blog = schema.get_blog(db=db, blog_id=blog_id)
     if db_blog is None:
         raise HTTPException(status_code=404, detail="Blog not found")
     return db_blog
 
-#  to get list of data 
-@router.get("/blogs/", response_model=list[schema.Blog])
-async def read_blogs(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-    return serializer.get_blogs(db=db, skip=skip, limit=limit)
+# create a new blog
+async def create_blog(blog: serializer.BlogCreate, db: Session):
+    return schema.create_blog(db=db, blog=blog)
 
+# get all blogs 
+async def get_blogs(skip: int, limit: int, db: Session):
+    blogs = db.query(Blog).offset(skip).limit(limit).all() 
+    return blogs
 
-@router.put("/blogs/{blog_id}", response_model=schema.Blog)
-async def update_blog(blog_id: int, blog: schema.BlogCreate, db: Session = Depends(get_db)):
-    db_blog = serializer.get_blog(db=db, blog_id=blog_id)
-    if db_blog is None:
-        raise HTTPException(status_code=404, detail="Blog not found")
-    
-    # Used for Update the blog
+# update a blog
+async def update_blog(blog_id: int, blog: serializer.BlogCreate, db: Session):
+    db_blog = await get_blog_by_id(blog_id, db) 
     db_blog.title = blog.title
     db_blog.content = blog.content
     db.commit()
     db.refresh(db_blog)
     return db_blog
 
-# to delete data
-@router.delete("/blogs/{blog_id}", response_model=schema.Blog)
-async def delete_blog(blog_id: int, db: Session = Depends(get_db)):
-    db_blog = serializer.get_blog(db=db, blog_id=blog_id)
-    if db_blog is None:
-        raise HTTPException(status_code=404, detail="Blog not found")
-    
+# delete a blog
+async def delete_blog(blog_id: int, db: Session):
+    db_blog = await get_blog_by_id(blog_id, db) 
     db.delete(db_blog)
     db.commit()
     return db_blog
--- src/blog/schema.py
+++ src/blog/schema.py
@@ -1,14 +1,15 @@
-from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from src.blog import model, serializer
 
-class BlogBase(BaseModel):
-    title: str
-    content: str
+def create_blog(db: Session, blog: serializer.BlogCreate):
+    db_blog = model.Blog(title=blog.title, content=blog.content)
+    db.add(db_blog)
+    db.commit()
+    db.refresh(db_blog)
+    return db_blog
 
-class BlogCreate(BlogBase):
-    pass
+def get_blog(db: Session, blog_id: int):
+    return db.query(model.Blog).filter(model.Blog.id == blog_id).first()
 
-class Blog(BlogBase):
-    id: int
-
-    class Config:
-        orm_mode = True
+def get_blogs(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(model.Blog).offset(skip).limit(limit).all()
